9b4ad214f512447c7352f3058f2722db54acbf96
JJimenez94 <JJimenez.9406@gmail.com>
2018-05-19 22:13:50 -0500

    Fixed code style errors

--- persistence/persistenceManager.py
+++ persistence/persistenceManager.py
@@ -17,45 +17,6 @@ class uploader:
             self.type = None
             print("Extensión invalida")
 
-    def checkBalance(self, dataset):
-        balance = dataset.groupby('class_col').data_col.count()
-        isbalanced = True
-        counter = 0
-        while(counter < (len(balance)-1) and isbalanced):
-            if (balance[counter] != balance[counter + 1]):
-                isbalanced = False
-        del balance, counter
-        return isbalanced
-
-    def removePunctuation(self, text):
-        punctuationSigns = list(punctuation)
-        punctuationSigns.extend(['¿', '¡'])
-        text = ''.join([c for c in text if c not in punctuationSigns])
-        del punctuationSigns
-        return text
-
-    def extractFeatures(self, dataset):
-        stop = stopwords.words('spanish')
-        stop.extend(stopwords.words('english'))
-        count_vect = CountVectorizer(
-            ngram_range=(1, 2), stop_words=stop, min_df=5)
-        tfidf_transformer = TfidfTransformer(sublinear_tf=True)
-        X_train, X_test, y_train, y_test = train_test_split(
-            dataset['data_col'], dataset['class_col'], random_state=0, train_size=0.7)
-        # Convirtiendo a vectores de caracteristicas
-        X_train_counts = count_vect.fit_transform(X_train)
-        X_train_tfidf = tfidf_transformer.fit_transform(X_train_counts)
-        X_test_counts = count_vect.transform(X_test)
-        X_test_tfidf = tfidf_transformer.fit_transform(X_test_counts)
-        del X_train, X_train_counts, stop, count_vect, tfidf_transformer, X_test, X_test_counts
-        return X_train_tfidf, X_test_tfidf, y_train, y_test
-
-    def balance(self, x, y):
-        sampler = RandomUnderSampler(random_state=0)
-        X_resampled, y_resampled = sampler.fit_sample(x, y)
-        del sampler
-        return X_resampled, y_resampled
-
     def uploadDataset(self, data_column, class_column):
         if self.type == ".csv" or self.type == ".txt":
             dataset = pandas.read_csv(
@@ -68,14 +29,57 @@ class uploader:
             return None
         print("Se cargó correctamente el dataset, limpiando...")
         dataset = dataset[pandas.notnull(dataset[data_column])]
-        dataset[data_column].apply(self.removePunctuation)
+        dataset[data_column].apply(removePunctuation)
         print("Limpieza exitosa")
         dataset.columns = ['class_col', 'data_col']
-        isbalanced = self.checkBalance(dataset)
-        X_train, X_test, y_train, y_test = self.extractFeatures(dataset)
+        isbalanced = checkBalance(dataset)
+        X_train, X_test, y_train, y_test = extractFeatures(dataset)
         if isbalanced == False:
             print("El dataset no está balanceado, rebalanceando...")
-            X_train, y_train = self.balance(X_train, y_train)
+            X_train, y_train = balance(X_train, y_train)
             print("Balanceo realizado exitosamente")
         del isbalanced, dataset
         return X_train, y_train, X_test, y_test
+
+
+def extractFeatures(dataset):
+    stop = stopwords.words('spanish')
+    stop.extend(stopwords.words('english'))
+    count_vect = CountVectorizer(
+        ngram_range=(1, 2), stop_words=stop, min_df=5)
+    tfidf_transformer = TfidfTransformer(sublinear_tf=True)
+    X_train, X_test, y_train, y_test = train_test_split(
+        dataset['data_col'], dataset['class_col'], random_state=0, train_size=0.7)
+    # Convirtiendo a vectores de caracteristicas
+    X_train_counts = count_vect.fit_transform(X_train)
+    X_train_tfidf = tfidf_transformer.fit_transform(X_train_counts)
+    X_test_counts = count_vect.transform(X_test)
+    X_test_tfidf = tfidf_transformer.fit_transform(X_test_counts)
+    del X_train, X_train_counts, stop, count_vect, tfidf_transformer, X_test, X_test_counts
+    return X_train_tfidf, X_test_tfidf, y_train, y_test
+
+
+def removePunctuation(text):
+    punctuationSigns = list(punctuation)
+    punctuationSigns.extend(['¿', '¡'])
+    text = ''.join([c for c in text if c not in punctuationSigns])
+    del punctuationSigns
+    return text
+
+
+def checkBalance(dataset):
+    balance = dataset.groupby('class_col').data_col.count()
+    isbalanced = True
+    counter = 0
+    while(counter < (len(balance)-1) and isbalanced):
+        if (balance[counter] != balance[counter + 1]):
+            isbalanced = False
+    del balance, counter
+    return isbalanced
+
+
+def balance(x, y):
+    sampler = RandomUnderSampler(random_state=0)
+    X_resampled, y_resampled = sampler.fit_sample(x, y)
+    del sampler
+    return X_resampled, y_resampled
